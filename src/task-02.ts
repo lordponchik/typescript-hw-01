395ae0739a2a20abab7df7682bc3056d4f143ae8
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-11-17 12:52:14 +0100

    task-02

---
+++ src/task-02.ts
@@ -0,0 +1,15 @@
+export {};
+
+// let anything = -20;
+// anything = 'Text';
+// anything = {};
+
+let anything: any = -20;
+anything = 'Text';
+anything = {};
+
+//
+
+let anything_1: number | string | object = -20;
+anything_1 = 'Text';
+anything_1 = {};
