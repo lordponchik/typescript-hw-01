86196ad6d92747b3837f38d3cd5892bf8601636a
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-11-17 12:48:49 +0100

    task-01

--- src/task-01.ts
+++ src/task-01.ts
@@ -1,1 +1,18 @@
+export {};
+// let age = 50;
+// let name = 'Max';
+// let toggle = true;
+// let empty = null;
+// let notInitialize;
+// let callback = a => {
+//   return 100 + a;
+// };
 
+let age: number = 5;
+let name: string = 'Max';
+let toggle: boolean = true;
+let empty: null = null;
+let notInitialize: undefined;
+let callback: object = (a: any) => {
+  return 100 + a;
+};
